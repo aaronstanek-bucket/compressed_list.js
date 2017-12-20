9d17dcc12697fb6f7932350a6ffd94cab83481e2
Aaron Stanek <nope>
2017-12-19 19:26:43 -0800

    made master Nick happy

--- compressed_list.js
+++ compressed_list.js
@@ -1,18 +1,18 @@
 function compress(v) {
-  r = v.toString();
-  b = document.getElementById("clb-"+r).innerHTML = "<input type=\"button\" value=\"+\" onclick=\"decompress("+r+")\">";
-  t = document.getElementById("clt-"+r).style.display = "none";
+  var r = v.toString();
+  var b = document.getElementById("clb-"+r).innerHTML = "<input type=\"button\" value=\"+\" onclick=\"decompress("+r+")\">";
+  var t = document.getElementById("clt-"+r).style.display = "none";
 };
 
 function decompress(v) {
-  r = v.toString();
-  b = document.getElementById("clb-"+r).innerHTML = "<input type=\"button\" value=\"-\" onclick=\"compress("+r+")\">";
-  t = document.getElementById("clt-"+r).style.display = "inline";
+  var r = v.toString();
+  var b = document.getElementById("clb-"+r).innerHTML = "<input type=\"button\" value=\"-\" onclick=\"compress("+r+")\">";
+  var t = document.getElementById("clt-"+r).style.display = "inline";
 };
 
 function compress_all() {
-  h = document.getElementsByTagName("span");
-  for (i=0;i<h.length;i++) {
+  var h = document.getElementsByTagName("span");
+  for (let i=0;i<h.length;i++) {
     if (h[i].id=="") {continue;};
     if (h[i].id.slice(0,4)=="clb-") {
       compress(h[i].id.slice(4));
